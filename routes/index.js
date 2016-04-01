05c629afdf9022f37bd5401decd941eec4db5f7d
Lauren Zugai <lauren@zugai.com>
2016-04-01 12:02:28 -0500

    looping through liked corgis on view

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,17 @@ var Corgi = require('../models/corgi');
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cool Corgis' });
+  Corgi.find(function(err, corgi) {
+     if (err) console.log(err);
+     res.render('index', { title: 'Cool Corgis', corgi: corgi });
+  });
 });
 
 router.get('/liked', function(req, res, next) {
-  res.render('liked', { title: 'Liked Corgis' });
+  Corgi.find(function(err, corgi) {
+   if (err) console.log(err);
+   res.render('liked', { title: 'Liked Corgis', corgi: corgi });
+  });
 });
 
 module.exports = router;
