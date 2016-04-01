0a3f19078745384e76219e35cbb623d5f73b64f8
Lauren Zugai <lauren@zugai.com>
2016-04-01 11:32:09 -0500

    Liked

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 
 var app = express();
@@ -28,7 +27,6 @@ var mongoose = require('mongoose');
 mongoose.connect(process.env.DB_CONN_CORGI);
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Corgi = require('../models/corgi');
 /* GET home page. */
 
-
-
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Cool Corgis' });
 });
 
-
-
+router.get('/liked', function(req, res, next) {
+  res.render('liked', { title: 'Liked Corgis' });
+});
 
 module.exports = router;
