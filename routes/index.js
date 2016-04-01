23b7651af8bbd29a7f28c642b2e84a4845c30b2a
Lauren Zugai <lauren@zugai.com>
2016-04-01 14:10:21 -0500

    Got like/dislike to work

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var Corgi = require('../models/corgi');
 router.get('/', function(req, res, next) {
   Corgi.find(function(err, corgi) {
      if (err) console.log(err);
-     res.render('index', { title: 'Cool Corgis', corgi: corgi[Math.floor(Math.random() * 3)] });
+     res.render('index', { title: 'Cool Corgis', corgi: corgi[Math.floor(Math.random() * corgi.length)] });
   });
 });
 
@@ -15,6 +15,24 @@ router.get('/liked', function(req, res, next) {
    if (err) console.log(err);
    res.render('liked', { title: 'Liked Corgis', corgi: corgi });
   });
+});
+
+router.post('/', function(req, res, next) {
+  var id = req.body.id;
+  var makeTrue = req.body.makeTrue;
+  var makeFalse = req.body.makeTrue;
+
+  if (makeTrue != null) {
+    Corgi.findByIdAndUpdate(id, { liked: true }, function(err, user) {
+      if (err) console.log(err);
+      res.redirect('/');
+    });
+  } else if (makeTrue != null) {
+    Corgi.findByIdAndUpdate(id, { liked: false }, function(err, user) {
+      if (err) console.log(err)
+      res.redirect('/');
+    });
+  }
 });
 
 module.exports = router;
